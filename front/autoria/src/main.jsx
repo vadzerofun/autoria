0faa27f80a38a32fd4845d2723f85f102a3b8a72
vadzerofun <vadzerofun@gmail.com>
2024-07-08 17:39:05 +0200

    Register and Login Form Design

--- front/autoria/src/main.jsx
+++ front/autoria/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Root from "./routes/root";
+import ErrorPage from './error-page.jsx';
+import { Register } from './Components/Register/Register.jsx';
+import { Login } from './Components/Login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/login",
+    element: <div><Login /></div>,
+  },
+  {
+    path: "/register",
+    element: <div><Register /></div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
